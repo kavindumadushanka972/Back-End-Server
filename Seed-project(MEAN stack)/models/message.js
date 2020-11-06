5a887ec65e51a9e96c7abb7751a4c7b1602d109a
kavindumadushanka972 <kavindumadushanka972@gmail.com>
2020-11-06 18:27:35 +0530

    init

--- Seed-project(MEAN stack)/models/message.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var schema = new Schema({
-    content: {type: String, required: true},
-    user: {type: Schema.Types.ObjectId, ref: 'User'} //mongoDB automatically creates an ID
-});
-
-module.exports = mongoose.model('Message', schema); //this will automatically create a collection called 'messages'
--- Seed-project(MEAN stack)/models/user.js
+++ Seed-project(MEAN stack)/models/user.js
@@ -6,7 +6,6 @@ var schema = new Schema({
     lastName: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
 });
 
 schema.plugin(mongooseUniqueValidator);
